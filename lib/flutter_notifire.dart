0c26337be4423c546aa2babeeb6ee326e364a025
Anil Chaudhari <imanilchaudhari@gmail.com>
2022-05-30 15:39:09 +0545

    initial commit

---
+++ lib/flutter_notifire.dart
@@ -0,0 +1,7 @@
+library flutter_notifire;
+
+/// A FlutterNotifire.
+class FlutterNotifire {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
---
+++ test/flutter_notifire_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_notifire/flutter_notifire.dart';
+
+void main() {
+  test('adds one to input values', () {
+    // final obj = FlutterNotifire();
+    // expect(obj.addOne(2), 3);
+    // expect(obj.addOne(-7), -6);
+    // expect(obj.addOne(0), 1);
+  });
+}
